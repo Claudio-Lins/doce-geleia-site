665ca664a3ebe7830abe9a6ea231dd8d74f851b9
Claudio Lins <claudio.lins@me.com>
2023-10-05 14:00:39 +0100

    pnpm dlx shadcn-ui@latest add

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,8 +16,6 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        tertiary:
-          "bg-gradient-to-r from-blue-400 to-blue-600 text-white hover:bg-blue-800",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
