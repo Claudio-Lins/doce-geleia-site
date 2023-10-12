3e8ee7cadcefa6aa42a9e1bd50ceb2817e3c15a5
Claudio Lins <claudio.lins@me.com>
2023-10-12 16:32:05 +0100

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "@/components/ProductCard"
+import { ProductCard } from "@/components/ProductCard"
 import React from "react"
 import prisma from "@/lib/prisma"
 import { Separator } from "@/components/ui/separator"
