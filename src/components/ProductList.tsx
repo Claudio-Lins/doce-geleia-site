dcc9408712c8512c5598a7ac06f1a4a6a4bb4b40
Claudio Lins <claudio.lins@me.com>
2023-10-12 18:02:09 +0100

    ...

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,8 +1,8 @@
 "use client"
-
-import { usePathname } from "next/navigation"
+import { Key } from "react"
+import { ProductCard } from "./_ProductCard"
+import { useRouter, usePathname, useSearchParams } from "next/navigation"
 import { Product } from "@/@types"
-import { ProductCard } from "./ProductCard"
 
 interface ProductListProps {
   products: Product[]
@@ -14,7 +14,7 @@ export function ProductList({ products }: ProductListProps) {
     <div className="flex flex-wrap gap-2 items-center justify-center">
       {products
         .filter((product: Product) => product.slug !== pathname.slice(10))
-        .map((product: Product) => (
+        .map((product: { id: Key | null | undefined }) => (
           <ProductCard key={product.id} product={product} />
         ))}
     </div>
