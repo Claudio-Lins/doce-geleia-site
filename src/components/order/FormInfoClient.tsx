4faeb0b88aceadaad40c046f4d43269ac99282dd
Claudio Lins <claudio.lins@me.com>
2023-10-22 10:58:00 +0100

    fix: zod error

--- src/components/order/FormInfoClient.tsx
+++ src/components/order/FormInfoClient.tsx
@@ -10,11 +10,11 @@ const formInfoClientSchema = z.object({
   firstName: z.string().min(3),
   lastName: z.string().min(3),
   email: z.string().email(),
-  phone: z.string(),
-  postalCode: z.string(),
-  address: z.string(),
-  city: z.string(),
-  country: z.string(),
+  phone: z.string().min(12),
+  postalCode: z.string().min(4),
+  address: z.string().min(3),
+  city: z.string().min(3),
+  country: z.string().default("portugal"),
   complement: z.string(),
   observations: z.string(),
 });
@@ -55,105 +55,143 @@ export function FormInfoClient() {
   }
 
   return (
-    <div className="w-full p-2 rounded-md flex flex-col items-center">
+    <div className="flex w-full flex-col items-center rounded-md p-2">
       <form
         id="formInfoClient"
         onSubmit={handleSubmit(handleFormInfoClient)}
-        className="w-full flex flex-col gap-2"
+        className="flex w-full flex-col gap-2"
       >
-        <div className="flex flex-col md:flex-row items-center w-full gap-2">
-          <Input
-            type="text"
-            placeholder="Primeiro Nome"
-            className="w-full"
-            {...register("firstName")}
-          />
-          <Input
-            type="text"
-            placeholder="Último Nome"
-            className="w-full"
-            {...register("lastName")}
-          />
+        <div className="flex w-full flex-col items-center gap-2 md:flex-row">
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Primeiro Nome"
+              className="w-full"
+              {...register("firstName")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.firstName && "Campo obrigatório"}
+            </small>
+          </div>
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Apelido"
+              className="w-full"
+              {...register("lastName")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.lastName && "Campo obrigatório"}
+            </small>
+          </div>
         </div>
-        <div className="flex items-center w-full gap-2">
-          <Input
-            type="email"
-            placeholder="Email"
-            className="w-full"
-            {...register("email")}
-          />
-          <Controller
-            control={control}
-            name="phone"
-            rules={{ required: true }}
-            render={({ field }) => (
-              <PhoneInput
-                {...field}
-                inputStyle={{
-                  color: "rgb(19, 19, 27)",
-                  height: "42px",
-                  fontSize: "14px",
-                  fontWeight: "400",
-                  fontSmooth: "auto",
-                  width: "100%",
-                  borderRadius: "6px",
-                  border: "1px solid #e2e8f0",
-                  outline: "none",
-                }}
-                containerStyle={{ margin: "0px" }}
-                buttonStyle={{}}
-                dropdownStyle={{ height: "150px" }}
-                country={"pt"}
-                onChange={field.onChange} // Use field.onChange para atualizar o valor
-                inputProps={{
-                  name: "phone",
-                  required: true,
-                  autoFocus: true,
-                }}
-              />
-            )}
-          />
+        <div className="flex w-full items-center gap-2">
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="email"
+              placeholder="Email"
+              className="w-full"
+              {...register("email")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.email && "Campo obrigatório"}
+            </small>
+          </div>
+          <div className="itmes-cneter flex w-full flex-col">
+            <Controller
+              control={control}
+              name="phone"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <PhoneInput
+                  {...field}
+                  inputStyle={{
+                    color: "rgb(19, 19, 27)",
+                    height: "42px",
+                    fontSize: "14px",
+                    fontWeight: "400",
+                    fontSmooth: "auto",
+                    width: "100%",
+                    borderRadius: "6px",
+                    border: "1px solid #e2e8f0",
+                    outline: "none",
+                  }}
+                  containerStyle={{ margin: "0px" }}
+                  buttonStyle={{}}
+                  dropdownStyle={{ height: "150px" }}
+                  country={"pt"}
+                  onChange={field.onChange} // Use field.onChange para atualizar o valor
+                  inputProps={{
+                    name: "phone",
+                    required: true,
+                    autoFocus: true,
+                  }}
+                />
+              )}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.phone && "Campo obrigatório"}
+            </small>
+          </div>
         </div>
 
-        <div className="flex items-center w-full gap-2">
-          <Input
-            type="text"
-            placeholder="Morada"
-            className="w-full"
-            {...register("address")}
-          />
+        <div className="flex w-full items-center gap-2">
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Morada"
+              className="w-full"
+              {...register("address")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.address && "Campo obrigatório"}
+            </small>
+          </div>
         </div>
-        <div className="flex items-center w-full gap-2">
-          <Input
-            type="text"
-            placeholder="Conselho"
-            className="w-full"
-            {...register("city")}
-          />
+        <div className="flex w-full items-center gap-2">
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Conselho"
+              className="w-full"
+              {...register("city")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.city && "Campo obrigatório"}
+            </small>
+          </div>
+
           <select
             placeholder="País"
-            className="w-full rounded-md border text-sm border-gray-300 p-2"
+            className="w-full rounded-md border border-gray-300 p-2 text-sm"
             {...register("country")}
           >
             <option value="portugal">Portugal</option>
             <option value="outro">Outro</option>
           </select>
         </div>
-        <div className="flex flex-col md:flex-row items-center w-full gap-2">
-          <Input
-            type="text"
-            placeholder="Código Postal"
-            className="w-full"
-            {...register("postalCode")}
-          />
-          <Input
-            type="text"
-            placeholder="Complemento"
-            className="w-full"
-            {...register("complement")}
-          />
+        <div className="flex w-full flex-col items-center gap-2 md:flex-row">
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Código Postal"
+              className="w-full"
+              {...register("postalCode")}
+            />
+            <small className="mt-2 text-red-600">
+              {errors.postalCode && "Campo obrigatório"}
+            </small>
+          </div>
+          <div className="itmes-cneter flex w-full flex-col">
+            <Input
+              type="text"
+              placeholder="Complemento"
+              className="w-full"
+              {...register("complement")}
+            />
+          </div>
         </div>
-        <div className="flex items-center w-full gap-2">
+        <div className="flex w-full items-center gap-2">
           <Textarea
             placeholder="Observações"
             className="w-full"
