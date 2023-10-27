e3f571b3dfa43fe95d35748ecabcb1b31021bcf2
Claudio Lins <claudio.lins@me.com>
2023-10-27 15:46:39 +0100

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,21 @@
 import { ProductCard } from "@/components/ProductCard";
 import { Separator } from "@/components/ui/separator";
-import { api } from "@/lib/api";
+import { prisma } from "@/lib/prisma";
 
-async function getAllProducts() {
-  const response = await api("/products");
-  const products = await response.json();
-  return products;
-}
+// async function getAllProducts() {
+//   const response = await api("/products");
+//   const products = await response.json();
+//   return products;
+// }
 
 export default async function Products() {
-  const products = await getAllProducts();
+  const products = await prisma.product.findMany({
+    include: {
+      productDetail: true,
+      category: true,
+      ingredients: true,
+    },
+  });
 
   return (
     <div className="mx-auto flex w-full max-w-6xl flex-col justify-center px-2 pb-12 pt-24 sm:p-0 md:h-screen">
