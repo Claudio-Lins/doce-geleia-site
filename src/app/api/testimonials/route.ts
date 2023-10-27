d2998633f6cc68e8bc12be10a4d2294b33b363c6
Claudio Lins <claudio.lins@me.com>
2023-10-27 17:05:44 +0100

    Update route.ts

--- src/app/api/testimonials/route.ts
+++ src/app/api/testimonials/route.ts
@@ -2,10 +2,6 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const testimonials = await prisma.testimonial.findMany({
-    where: {
-      published: true,
-    },
-  });
+  const testimonials = await prisma.testimonial.findMany();
   return NextResponse.json(testimonials);
 }
