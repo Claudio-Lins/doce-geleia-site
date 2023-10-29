0dea5f33b5edddaf0862f2c26cf6cecf1a8af9fa
Claudio Lins <claudio.lins@me.com>
2023-10-29 11:55:33 +0000

    feat: image comic on cantacPage

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,19 @@
-import { ContactForm } from "@/components/email-template/ContactForm"
-import { Button } from "@/components/ui/button"
-import React from "react"
+import { ContactForm } from "@/components/email-template/ContactForm";
+import Image from "next/image";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center ">
+    <div className="flex min-h-screen w-full flex-wrap items-center justify-center pt-20">
+      <div className="w-1/2">
+        <Image
+          src="/assets/karen-comic.jpg"
+          alt="contact"
+          width={500}
+          height={500}
+          className="rounded-full"
+        />
+      </div>
       <ContactForm />
     </div>
-  )
+  );
 }
